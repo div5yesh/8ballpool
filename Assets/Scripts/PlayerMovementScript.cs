68f9124f69544a025ca47fb5ec073440c5bacf4d
Divyesh Chitroda <div5yesh@gmail.com>
2017-06-06 23:00:56 +0530

    add pool table and single player control with keyboard

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    Rigidbody rigidbody;
+
+    public float force = 5;
+    // Use this for initialization
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(x, 0, y);
+        rigidbody.AddForce(movement*force);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.name == "")
+        {
+
+        }
+    }
+}
