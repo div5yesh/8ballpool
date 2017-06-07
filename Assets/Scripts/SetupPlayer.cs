6ba4d3f2794be77c94d76718f3a0c91829343c50
Divyesh Chitroda <div5yesh@gmail.com>
2017-06-07 21:05:07 +0530

    add colored balls and multiplayer cue ball with keyboard controlling

---
+++ Assets/Scripts/SetupPlayer.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupPlayer : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        if (isLocalPlayer)
+        {
+            GetComponent<PlayerMovementScript>().enabled = true;
+        }
+		
+	}
+}
---
+++ Assets/Scripts/ShootCue.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class ShootCue : MonoBehaviour, IPointerClickHandler{
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Vector3 pos = eventData.pointerCurrentRaycast.worldPosition;
+        int x = Mathf.FloorToInt(pos.x);
+        int z = Mathf.FloorToInt(pos.z);
+        Debug.Log(x + "" + z);
+    }
+
+    // Use this for initialization
+    void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
