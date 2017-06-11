85dd1951dc3fed26276939e27697da955fbea4cb
Divyesh Chitroda <div5yesh@gmail.com>
2017-06-12 00:40:41 +0530

    add "Pool Billiard Table" assets
     - WIP: multiplayer scripts

--- Assets/Scripts/ShootCue.cs
+++ Assets/Scripts/ShootCue.cs
@@ -3,23 +3,47 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Networking;
 
-public class ShootCue : MonoBehaviour, IPointerClickHandler{
+public class ShootCue : NetworkBehaviour, IPointerClickHandler
+{
     public void OnPointerClick(PointerEventData eventData)
     {
-        Vector3 pos = eventData.pointerCurrentRaycast.worldPosition;
-        int x = Mathf.FloorToInt(pos.x);
-        int z = Mathf.FloorToInt(pos.z);
-        Debug.Log(x + "" + z);
+        //Vector3 pos = eventData.pointerCurrentRaycast.worldPosition;
+        //int x = Mathf.FloorToInt(pos.x);
+        //int z = Mathf.FloorToInt(pos.z);
     }
 
+    public bool playerTurn = false;
+    //private NetworkStartPosition spawnPoint;
     // Use this for initialization
-    void Start () {
+    //void Start()
+    //{
+    //    if (isLocalPlayer)
+    //    {
+    //        Vector3 cuePosition = GameObject.FindWithTag("cueBall").transform.position;
+    //        SpawnPlayer(cuePosition);
+    //    }
+    //}
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    //[ClientRpc]
+    //void SpawnPlayer(Vector3 cuePosition)
+    //{
+    //    if (isLocalPlayer)
+    //    {
+    //        transform.position = new Vector3(cuePosition.x, cuePosition.y, cuePosition.z);
+    //    }
+    //}
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    //public override void OnStartLocalPlayer()
+    //{
+    //    //GetComponent<MeshRenderer>().material.color = Color.blue;
+
+    //}
 }
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SpawnManager : MonoBehaviour {
+
+    private GameObject cue;
+
+    public Object stickPrefab;
+
+    public GameObject SpawnStick(Vector3 position, NetworkHash128 assetId)
+    {
+        cue.transform.position = position;
+        cue.SetActive(true);
+        return cue;
+    }
+
+    public void UnspawnStick(GameObject spawned)
+    {
+        Debug.Log("unspawn object " + spawned.name);
+        spawned.SetActive(false);
+    }
+
+    void Init()
+    {
+        cue = (GameObject)Instantiate(stickPrefab, Vector3.zero, Quaternion.identity);
+    }
+    // Use this for initialization
+    void Start () {
+        Init();
+        NetworkHash128 stickAssetId = GetComponent<NetworkIdentity>().assetId;
+        ClientScene.RegisterSpawnHandler(stickAssetId, SpawnStick, UnspawnStick);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //NetworkHash128 creatureAssetId = NetworkHash128.Parse("e2656f");
+        //NetworkServer.Spawn(gameObject, creatureAssetId);
+    }
+}
